21c11ed01fb5bf05eea48f2da432a84fc517164e
Roman Bielokon <belokon.roman@gmail.com>
2020-06-30 19:29:15 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:first_web/constant.dart';
+import 'package:first_web/screen/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
